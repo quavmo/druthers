b17c27e912e61eb7862af137f7e88577e80e341e
Gavin Morgan <quavmo@gmail.com>
2015-12-07 16:07:35 -0800

    Gulp notifies post bundle

--- gulp/build.js
+++ gulp/build.js
@@ -13,15 +13,14 @@ function compile(watch) {
   var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
 
   function rebundle() {
-    console.log('bundling...');
-
     return bundler.bundle()
       .on('error', function(err) { console.error(err); this.emit('end'); })
       .pipe(source('build.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./build'));
+      .pipe(gulp.dest('./build'))
+      .pipe($.notify("Bundled!"));
   }
 
   watch && bundler.on('update', rebundle);
