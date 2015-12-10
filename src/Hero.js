50e5faabe8fd0326172a8dc5dc1dd4c0dc0ea6c6
Gavin Morgan <quavmo@gmail.com>
2015-12-10 12:18:16 -0800

    Hero and Signup are responsive
    - Hero fetches copy from firebase

--- src/Hero.js
+++ src/Hero.js
@@ -1,27 +1,44 @@
 import React from 'react';
+import Firebase from 'firebase';
+let heroBase = new Firebase("https://druthers-base.firebaseio.com/marketing/hero");
+let blue = 'rgba(0,84,255,1)';
 
 export default React.createClass({
+	getInitialState: function(){
+		return {
+			header: 'Quit bickering.',
+			subheader: "Settle decisions with an eye on everyone's priorities.",
+			action: "Get notified when it's released."
+		}
+	},
+	componentDidMount: function () {
+		heroBase.on('value', function (data) { this.setState(data.val()) }, this);
+	},
 	render: function() {
-    let header    = React.DOM.h1({style: {marginTop: 100, fontSize: 36}}, 'Quit bickering.');
-    let subheader = React.DOM.h1({style: {marginTop: 10, fontSize: 14}}, "Settle decisions with an eye on everyone's priorities.");
-    let action    = React.DOM.a({href: '#signUp', style: {color: 'white', textDecoration: 'none', marginTop: 30, padding: 20, display: 'inline-block', background: 'blue', textTransform: 'capitalize'}}, "Get notified when it's released.");
+    let header    = React.DOM.h1({style: {fontSize: 36}}, this.state.header);
+    let subheader = React.DOM.h1({style: {marginTop: 10, fontSize: 14}}, this.state.subheader);
+    let action    = React.DOM.a({href: '#signUp', style: {color: 'white', textDecoration: 'none', marginTop: 30, padding: 20, display: 'inline-block', background: blue, textTransform: 'capitalize'}}, this.state.action);
 		let shotUrl 	= 'https://s3.amazonaws.com/media.launchrockstaging.com/assets/sites/site-7v23ksjv6uwjl511cwkt3g93u/jlxz7r-phone.png';
 		let appShot   = React.DOM.img({src: shotUrl, style: {margin: '100px auto', maxWidth: 371, display: 'block'}});
+		let responsiveStyleBox = React.DOM.style(
+			{},
+			`
+				@media (max-width: 1000px) { #hero { padding: 50px; } }
+				@media (min-width: 1000px) { #hero { padding: 130px; } }
+			`
+		)
 
-		// let photoId = '45/PlEgx5PSoiiJOmnE2izQ_NYC%20skyline%20empire-1.jpg'
-		// let photoId = 'reserve/oY3ayprWQlewtG7N4OXl_DSC_5225-2.jpg';
-		let photoId = 'photo-1447752875215-b2761acb3c5d';
-    let bgndUri = 'https://images.unsplash.com/'+photoId+'?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=f2460c17083e439b73d250a7db2a889c';
-
-    let outerStyle  = {
-      background: 'url('+bgndUri+')',
-      backgroundSize: 'cover',
-      color: 'white',
-      padding: 20,
+    return React.DOM.div({id: 'hero', style: this.outerStyle}, responsiveStyleBox, header, subheader, action, appShot);
+	},
+	get photoId () { return 'photo-1447752875215-b2761acb3c5d'; },
+	get bgndUri () { return `https://images.unsplash.com/${this.photoId}?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=f2460c17083e439b73d250a7db2a889c`; },
+	get outerStyle () {
+		return {
+			background: 'url('+this.bgndUri+')',
+			backgroundSize: 'cover',
+			color: 'white',
 			height: 550,
 			overflow: 'hidden'
-    };
-
-    return React.DOM.div({style: outerStyle}, header, subheader, action, appShot);
+		};
 	}
 });
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import Firebase from 'firebase';
 let interestedBase = new Firebase("https://druthers-base.firebaseio.com/interested");
+let blue = 'rgba(0,84,255,1)';
 
 export default React.createClass({
 	getInitialState: function() { return {name: '', email: ''}; },
 	render: function() {
-		let header 		= React.DOM.h1({style: {textTransform: 'capitalize', fontSize: 42, marginTop: 45}}, "Get notified when it's released");
+		let header 		= React.DOM.h1({style: {textTransform: 'capitalize', fontSize: 42}}, "Get notified when it's released");
 		let subheader = React.DOM.p({style: {marginTop: 20}}, "You'll be able to create a ballot immediately, and it will be the last day your group priorities are unclear.");
 		let name 			= React.DOM.input(Object.assign(this.inputProps, {placeholder: "Name", onChange: this.handleNameChange, value: this.state.name}));
 		let email 		= React.DOM.input(Object.assign(this.inputProps, {placeholder: "Email", onChange: this.handleEmailChange, value: this.state.email}));
-		let submit 		= React.DOM.button({style: {margin: '20px auto', display: 'block'}}, "Submit");
 
-		let outerProps = {id: 'signUp', style: {padding: 20, color: 'white', background: 'rgba(0,84,255,1)', fontFamily: 'sans-serif'}}
+		let submit 		= React.DOM.button({style: {cursor: 'pointer', border: '3px solid white', padding: 20, color: 'white', textTransform: 'capitalize', margin: '20px auto', display: 'block'}}, "Submit");
+
+		let outerProps = {id: 'signUp', style: {color: 'white', background: blue, fontFamily: 'sans-serif'}}
+
+		let responsiveStyleBox = React.DOM.style(
+			{},
+			`
+				@media (max-width: 1000px) { #signUp { padding: 50px; } }
+				@media (min-width: 1000px) { #signUp { padding: 130px; } }
+				button { background: ${blue}; }
+				button:hover { background: grey; }
+			`
+		)
 
 		return React.DOM.div(
 			outerProps,
+			responsiveStyleBox,
 			header,
 			subheader,
 			React.DOM.style({}, '::-webkit-input-placeholder { color: white }'),
