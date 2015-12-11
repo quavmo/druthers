2ddfd68107b2ccaef3ba39b89158b3a158c1b229
Gavin Morgan <quavmo@gmail.com>
2015-12-11 13:47:42 -0800

    Thank visitor for submission

--- src/SignUp.js
+++ src/SignUp.js
@@ -7,10 +7,11 @@ export default React.createClass({
 	getInitialState: function() { return {name: '', email: ''}; },
 	render: function() {
 		let header 		= React.DOM.h1({style: {textTransform: 'capitalize', fontSize: 42}}, "Get notified when it's released");
+		let gratitude = React.DOM.h1({style: {marginTop: 50, textTransform: 'capitalize', fontSize: 42}}, `Rad, ${this.state.name}.`);
 		let subheader = React.DOM.p({style: {marginTop: 20}}, "You'll be able to create a ballot immediately, and it will be the last day your group priorities are unclear.");
+
 		let name 			= React.DOM.input(Object.assign(this.inputProps, {placeholder: "Name", onChange: this.handleNameChange, value: this.state.name}));
 		let email 		= React.DOM.input(Object.assign(this.inputProps, {placeholder: "Email", onChange: this.handleEmailChange, value: this.state.email}));
-
 		let submit 		= React.DOM.button({style: {cursor: 'pointer', border: '3px solid white', padding: 20, color: 'white', textTransform: 'capitalize', margin: '20px auto', display: 'block'}}, "Submit");
 
 		let outerProps = {id: 'signUp', style: {color: 'white', background: blue, fontFamily: 'sans-serif'}}
@@ -31,11 +32,11 @@ export default React.createClass({
 			header,
 			subheader,
 			React.DOM.style({}, '::-webkit-input-placeholder { color: white }'),
-			React.DOM.form({onSubmit: this.handleSubmit},
+			!this.state.submitted ? React.DOM.form({onSubmit: this.handleSubmit},
 				name,
 				email,
 				submit
-			)
+			) : gratitude
 		);
 	},
 	handleNameChange: function (e) {
@@ -49,7 +50,7 @@ export default React.createClass({
 	handleSubmit: function (e) {
 		e.preventDefault();
 		interestedBase.push(this.state);
-		this.setState({email: '', name: ''});
+		this.setState({submitted: true});
  	},
 	inputProps: {
 		type: 'text',
