17ec8cf2e155e3b762e3361c84dd47fc3a2183b2
Gavin Morgan <quavmo@gmail.com>
2015-12-16 10:20:44 -0800

    Move MarketingPage under src/marketing

--- gulp/test.js
+++ gulp/test.js
@@ -8,15 +8,13 @@ const $ = gulpLoadPlugins();
 let testCode    = 'test/**/*.js';
 let sourceCode  = 'src/**/*.js';
 
-
-
 gulp.task('test', function() {
    let testPipe = gulp.src(testCode);
    let notifier = { specDone: function(result) { console.log('this could be a notification'); } };
-   
+
    return testPipe.pipe($.jasmine({reporter: [new Reporter(), notifier]}));
 });
 
-gulp.task('watch-test', function () {
+gulp.task('watch-test', ['test'], function () {
   return gulp.watch([testCode, sourceCode], ['test']);
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import MarketingPage from './pages/marketing/Page.js';
 
-ReactDOM.render(React.createElement(App), document.getElementById('app'));
+ReactDOM.render(React.createElement(MarketingPage), document.getElementById('app'));
