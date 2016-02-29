a509a500d59586cb5a0f5252e11733567cb7618b
Gavin Morgan <quavmo@gmail.com>
2016-02-29 15:19:30 -0600

    turn global alpha off

--- src/pages/marketing/Hero.js
+++ src/pages/marketing/Hero.js
@@ -5,7 +5,7 @@ import CallToAction from './CallToAction';
 let druthersBase = new Firebase("https://druthers-base.firebaseio.com")
 let heroBase = druthersBase.child('marketing').child('hero');
 let ballotsBase = druthersBase.child('ballots');
-let alpha = true;
+let alpha = false;
 
 export default React.createClass({
 	getInitialState: function(){
