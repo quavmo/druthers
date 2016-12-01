ef5603a9a528dd23b3c94b167d3517a32c1793b8
Gavin Morgan <quavmo@gmail.com>
2016-12-01 11:23:42 -0800

    display ballotCount on resultsPage

--- pages/results/index.js
+++ pages/results/index.js
@@ -1,12 +1,14 @@
 import { pick } from 'ramda';
 import { connect } from 'react-redux';
 import { Component, DOM, createElement as el } from 'react';
-const { h2 } = DOM;
+const { h2, div } = DOM;
 import * as mapDispatchToProps from '../../core/actions';
 import LeaderBoard from '../../components/LeaderBoard';
 import Layout from '../../components/Layout'
 import Title from '../../components/Title';
 import { docketBase } from '../../core/services/DataService';
+import num from 'numeral';
+import { ballotCount } from './style.css';
 
 class ResultsPage extends Component {
   constructor(props) {
@@ -16,8 +18,17 @@ class ResultsPage extends Component {
   
   render() {
     const { ballots, members, title } = this.props.currentDocket;
+
+    const countString = ballots => {
+      const count = Object.keys(ballots).length;
+      const humanCount = num(count).format('0,0');
+      const noun = count == 1 ? 'ballot' : 'ballots';
+      return `( ${humanCount} ${noun} )`;
+    }
+     
     return el(Layout, {},
       h2({}, title),
+      div({className: ballotCount}, countString(ballots)),
       el(LeaderBoard, { members, ballots })
     );
   }
