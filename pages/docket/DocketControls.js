69a305d15ef16940b76b6b8caa4370b16a53ddc5
Gavin Morgan <quavmo@gmail.com>
2016-12-12 15:08:56 -0800

    Docket Controls is FABs

--- pages/docket/DocketControls.js
+++ pages/docket/DocketControls.js
@@ -1,30 +1,35 @@
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { RaisedButton } from 'material-ui';
+import { FloatingActionButton } from 'material-ui';
 import { DOM, createElement as el } from 'react';
+import { isEmpty } from 'ramda';
 import CopyIcon from 'material-ui/svg-icons/content/content-copy';
+import SaveIcon from 'material-ui/svg-icons/file/cloud-done';
 const { span, div } = DOM;
+import { fab } from './style.css'
 
 const newBallotPath = docketID => `${window.location.host}/dockets/${docketID}/ballots/new`;
 
-const DocketControls = ({ docket, finalizeDocket }) => {
-  const submitButton = el(RaisedButton,
-    {
-      onClick: event => {
-        event.preventDefault();
-        finalizeDocket(docket);
-      },
-      disabled: docket.finalizing,
-      label: 'Finalize and Share',
-    }
-  );
+const SubmitButton = ({finalizeDocket, docket}) => el(FloatingActionButton,
+  {
+    className: fab,
+    onTouchTap: () => finalizeDocket(docket),
+    disabled: docket.finalizing || !docket.title || docket.members.length < 2,
+    label: 'Finalize',
+  }, el(SaveIcon)
+);
 
-  const path = newBallotPath(docket.id);
-  const label = 'Copy Link to Share';
-  const urlCopyButton = el(CopyToClipboard, { text: path },
-      el(RaisedButton, { label, secondary: true, icon: el(CopyIcon) })
-  );
-  const urlWidget = div({}, urlCopyButton);
-  return span({}, (docket.id ? urlWidget : submitButton));
+const CopyButton = ({text}) => 
+el(CopyToClipboard, { text },
+  el(FloatingActionButton, { 
+    className: fab,
+    secondary: true
+  }, el(CopyIcon))
+);
+
+const DocketControls = ({ docket, finalizeDocket }) => {
+  return docket.id ?
+    el(CopyButton, {text: newBallotPath(docket.id)}) :
+    el(SubmitButton, { finalizeDocket, docket });
 };
 
 export default DocketControls;
