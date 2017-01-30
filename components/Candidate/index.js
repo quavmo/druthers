8c84275dbab2d67db88426e3f25d935a8df1788d
Gavin Morgan <quavmo@gmail.com>
2017-01-29 17:44:01 -0800

    only drag icon is drag target

--- components/Candidate/index.js
+++ components/Candidate/index.js
@@ -1,3 +1,5 @@
+import { createElement as el, DOM } from 'react';
+const { div } = DOM;
 import {
   Paper,
   ListItem,
@@ -6,18 +8,23 @@ import DragHandleIcon from 'material-ui/svg-icons/editor/drag-handle';
 import DeleteIcon from 'material-ui/svg-icons/action/delete';
 
 
-import {
-  createElement as el,
-} from 'react';
+const rightIcon = (deleteCandidate, text) =>
+  (deleteCandidate ? el(DeleteIcon, {
+    onClick: () => deleteCandidate(text),
+  }) : null);
 
+const leftIcon = (deleteCandidate, connectDragSource) =>
+  (!deleteCandidate ? connectDragSource(div({}, el(DragHandleIcon, {}))) : null);
 
-const Candidate = ({ isDragging, deleteCandidate, text }) =>
+const Candidate = ({
+  connectDragSource, isDragging, deleteCandidate, text,
+}) =>
 el(Paper, { zDepth: isDragging ? 3 : 1, style: { margin: 4 } },
   el(ListItem, {
     disableTouchRipple: !deleteCandidate,
     primaryText: text,
-    rightIcon: deleteCandidate ? el(DeleteIcon, { onClick: () => deleteCandidate(text) }) : null,
-    leftIcon: !deleteCandidate ? el(DragHandleIcon, {}) : null,
+    rightIcon: rightIcon(deleteCandidate, text),
+    leftIcon: leftIcon(deleteCandidate, connectDragSource),
   })
 );
 
